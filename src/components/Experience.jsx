0a2ab56d02d7b6cf9497c73366c63d766e6bc681
DhananjoyShaw <dhananjoyshaw0218@gmail.com>
2025-08-14 00:44:41 +0530

    intial commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -27,7 +27,7 @@ const ExperienceCard = ({ experience }) => {
           <img
             src={experience.icon}
             alt={experience.company_name}
-            className='w-[60%] h-[60%] bg-white object-contain'
+            className='w-[60%] h-[60%] object-contain'
           />
         </div>
       }
